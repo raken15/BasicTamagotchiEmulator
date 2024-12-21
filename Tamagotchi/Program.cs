9549118ed795e4a5e2b98c32f386e9a19dd76576
Raviv <raken100@gmail.com>
2024-12-21 23:48:25 +0200

    Added implementation of the main program and the calls to the actions on the pet

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Tamagotchi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your Tamagotchi's name:");
+            string name = Console.ReadLine() ?? string.Empty;
+            if(string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Invalid name. Quitting the program");
+                return;
+            }
+            TamagotchiLogic tamagotchi = new TamagotchiLogic(name);
+            bool isAlive = true;
+
+            while(isAlive)
+            {
+                Console.Clear();
+                tamagotchi.ShowStatus();
+                Console.WriteLine("What do you want to do? (Feed, Play, Sleep, Show Status, Quit)");
+                if(!tamagotchi.CurrentlyUpdating)
+                {
+                    string choice = Console.ReadLine() ?? string.Empty;
+                    switch (choice.ToLower())
+                    {
+                        case "feed":
+                            tamagotchi.Feed();
+                            System.Threading.Thread.Sleep(3000);
+                            break;
+                        case "play":
+                            tamagotchi.Play();
+                            System.Threading.Thread.Sleep(3000);
+                            break;
+                        case "sleep":
+                            tamagotchi.Sleep();
+                            System.Threading.Thread.Sleep(3000);
+                            break;
+                        case "show status":
+                            tamagotchi.ShowStatus();
+                            System.Threading.Thread.Sleep(3000);
+                            break;
+                        case "Quit":
+                            Console.WriteLine("Goodbye!");
+                            isAlive = false;
+                            return;
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
